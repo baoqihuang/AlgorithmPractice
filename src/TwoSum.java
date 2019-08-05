3a823408882469fbe0bb2d8f410bf75988fb5429
Michael Huang <hbqljy@gmail.com>
2019-08-04 17:59:23 -0700

    3

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -5,5 +5,22 @@ public class TwoSum {
 		// TODO Auto-generated method stub
 
 	}
+	public boolean existSum(int[] array, int target) {
+	    // Write your solution here
+	    int n = array.length;
+	    int left = 0; 
+	    int right = n - 1;
+	    boolean found = false;
+	    while(!found && left < right) {
+	      if (array[left] + array[right] < target) {
+	        left++;
+	      } else if (array[left] + array[right] > target) {
+	        right--;
+	      } else {
+	        return true;
+	      }
+	    }
+	    return found;
+	}
 
 }
